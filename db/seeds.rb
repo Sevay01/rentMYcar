f368d15617c6ae393467a222e4182bdfcb43405f
Yves MOMBELLET <y.mombellet@gmail.com>
2022-04-22 15:24:08 +0200

    Seeds et Controleur OK

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,17 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+User.destroy_all
+Car.destroy_all
+Booking.destroy_all
+puts "Seeds destroyed"
+
+user1 = User.create(email: "toto@gmail.com", password: "123456", password_confirmation: "123456" )
+user2 = User.create(email: "pat@gmail.com", password: "123456", password_confirmation: "123456" )
+
+puts "Users created..."
+
+Car.create(  brand: "renault", model: "zoe", details: "Voiture Francaise pas chère", price: 100)
+Car.create(  brand: "Peugeot", model: "3008", details: "Voiture Francaise Moyen de Gamme", price: 800)
+Car.create(  brand: "Porshe", model: "911", details: "Voiture de fonction", price: 2000)
+puts "Cars created..."
